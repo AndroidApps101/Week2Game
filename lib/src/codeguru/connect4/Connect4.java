6a44cfbaa55d4fb75dad41d86e2fdff73cba169e
codeguru <codeguru@users.sourceforge.net>
2013-12-14 12:49:44 -0700

    Blue background

--- lib/src/codeguru/connect4/Connect4.java
+++ lib/src/codeguru/connect4/Connect4.java
@@ -1,23 +1,27 @@
 package codeguru.connect4;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
 public class Connect4 implements ApplicationListener {
-	public void create () {
+	public void create() {
 	}
 
-	public void render () {
+	public void render() {
+		Gdx.gl.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 
-	public void resize (int width, int height) {
+	public void resize(int width, int height) {
 	}
 
-	public void pause () {
+	public void pause() {
 	}
 
-	public void resume () {
+	public void resume() {
 	}
 
-	public void dispose () {
+	public void dispose() {
 	}
 }
