86b801783d93a6bca171dc76b022b1c18b709a35
codeguru <codeguru@users.sourceforge.net>
2013-12-14 16:23:20 -0700

    Initialize an empty board

--- lib/src/codeguru/connect4/Board.java
+++ lib/src/codeguru/connect4/Board.java
@@ -2,4 +2,22 @@ package codeguru.connect4;
 
 public class Board {
 
+	public enum State { EMPTY, PLAYER1, PLAYER2};
+	public static int ROW_COUNT = 6;
+	public static int COL_COUNT = 7;
+	
+	private State[][] board = new State[ROW_COUNT][COL_COUNT];
+	private int[] next = new int[COL_COUNT];
+	
+	public Board() {
+		for (int i = 0; i < ROW_COUNT; ++i) {
+			for (int j = 0; j < COL_COUNT; ++j) {
+				this.board[i][j] = State.EMPTY;
+			}
+		}
+	}
+	
+	public State getState(int row, int col) {
+		return board[row][col];
+	}
 }
