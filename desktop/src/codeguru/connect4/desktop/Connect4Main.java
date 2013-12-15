b9eb9644827dddf53328ea372457192b3ec70b87
codeguru <codeguru@users.sourceforge.net>
2013-12-14 18:36:20 -0700

    Render empty board

--- desktop/src/codeguru/connect4/desktop/Connect4Main.java
+++ desktop/src/codeguru/connect4/desktop/Connect4Main.java
@@ -1,10 +1,13 @@
 package codeguru.connect4.desktop;
 
+import codeguru.connect4.Board;
 import codeguru.connect4.Connect4;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class Connect4Main {
-	public static void main (String[] args) {
-		new LwjglApplication(new Connect4(), "Connect4", 480, 320, false);
-	}
+    public static void main(String[] args) {
+        new LwjglApplication(new Connect4(), "Connect4",
+                (int) (2 * Connect4.RADIUS * Board.COL_COUNT),
+                (int) (2 * Connect4.RADIUS * Board.ROW_COUNT), false);
+    }
 }
--- lib/src/codeguru/connect4/Connect4.java
+++ lib/src/codeguru/connect4/Connect4.java
@@ -2,41 +2,63 @@ package codeguru.connect4;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.Mesh;
-import com.badlogic.gdx.graphics.VertexAttribute;
-import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Connect4 implements ApplicationListener {
-	private Mesh mesh = null;
-	
-	public void create() {
-		if (this.mesh == null) {
-			this.mesh = new Mesh(true, 3, 3, new VertexAttribute(Usage.Position, 3,
-					"a_position"));
+    public static float RADIUS = 25.0f;
+    
+    private ShapeRenderer renderer = null;
+    private Board board = null;
 
-			this.mesh.setVertices(new float[] { -0.5f, -0.5f, 0, 0.5f, -0.5f, 0, 0,
-					0.5f, 0 });
-			this.mesh.setIndices(new short[] { 0, 1, 2 });
-		}
-	}
+    public void create() {
+        if (this.renderer == null) {
+            this.renderer = new ShapeRenderer();
+        }
 
-	public void render() {
-		Gdx.gl.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		mesh.render(GL10.GL_TRIANGLES, 0, 3);
-	}
+        if (this.board == null) {
+            this.board = new Board();
+        }
+    }
 
-	public void resize(int width, int height) {
-	}
+    public void render() {
+        Gdx.gl.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-	public void pause() {
-	}
+        renderer.begin(ShapeType.Filled);
+        for (int row = 0; row < Board.ROW_COUNT; ++row) {
+            for (int col = 0; col < Board.COL_COUNT; ++col) {
+                switch (this.board.getState(row, col)) {
+                case EMPTY:
+                    renderer.setColor(Color.WHITE);
+                    break;
+                case PLAYER1:
+                    renderer.setColor(Color.RED);
+                    break;
+                case PLAYER2:
+                    renderer.setColor(Color.RED);
+                    break;
+                }
+                
+                float x = 2 * col * RADIUS + RADIUS;
+                float y = 2 * row * RADIUS + RADIUS;
+                renderer.circle(x, y, RADIUS);
+            }
+        }
+        renderer.end();
+    }
 
-	public void resume() {
-	}
+    public void resize(int width, int height) {
+    }
 
-	public void dispose() {
-	}
+    public void pause() {
+    }
+
+    public void resume() {
+    }
+
+    public void dispose() {
+    }
 }
